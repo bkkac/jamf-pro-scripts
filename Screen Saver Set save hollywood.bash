a3703f341e9918f16f75cc5cd402d170af73cdc0
Will Pierce <will.pierce@collemcvoy.com>
2019-07-18 15:30:42 -0500

    Update Screen Saver Set save hollywood.bash
    updated Get cutent loged in user, added some comments

--- Screen Saver Set save hollywood.bash
+++ Screen Saver Set save hollywood.bash
@@ -1,18 +1,22 @@
 #!/bin/bash
-
-#  Set screensaver to SaveHollywood with setings to use videos in:
-#  /Users/Shared/ScreenSaverVideos
+# Will Pierce
+# 2019 07 18
+# https://www.jamf.com/jamf-nation/discussions/31201/setting-custom-screensaver
+# Set screensaver to SaveHollywood with setings to use videos in:
+# /Users/Shared/ScreenSaverVideos
 
 # Get user logged into console and put into variable "user"
-user=`ls -l /dev/console | cut -d " " -f 4`
-osMajor=$(sw_vers -productVersion | awk -F"." '{print $2}')
-osMinor=$(sw_vers -productVersion | awk -F"." '{print $3}')
+user=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+# If you plan on deploying to mac os other than 14 (Mojave) then check the mac os
+# osMajor=$(sw_vers -productVersion | awk -F"." '{print $2}')
+# osMinor=$(sw_vers -productVersion | awk -F"." '{print $3}')
 
 sudo -u $user defaults -currentHost write com.apple.screensaver CleanExit -string "YES"
 sudo -u $user defaults -currentHost write com.apple.screensaver PrefsVersion -int 100
 sudo -u $user defaults -currentHost write com.apple.screensaver showClock -string "NO"
 sudo -u $user defaults -currentHost write com.apple.screensaver idleTime -int 600
 
+# Again if older then Mojave
 # if [[ $osMajor -eq 14 ]] && [[ $osMinor -ge 2 ]]; then
 # sudo -u $user defaults -currentHost write com.apple.screensaver moduleDict -dict moduleName -string "iLifeSlideshows" path -string "/System/Library/Frameworks/ScreenSaver.framework/PlugIns/iLifeSlideshows.appex" type -int 0
 # else
