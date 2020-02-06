944538e6bd95c3f93730be994300cdbda67092a1
Will Pierce <qday1@me.com>
2020-02-06 14:07:54 -0600

    removed old references

--- AdobeSerialization_XX-REMOVE.sh
+++ AdobeSerialization_XX-REMOVE.sh
@@ -5,7 +5,7 @@ pathToScript=$0
 pathToPackage=$1
 targetLocation=$2
 targetVolume=$3
-
+#
 /private/tmp/Adobe_License_XX/RemoveVolumeSerial  #runs the Adobe Serialization package
 
 # if [ -d /private/tmp/Adobe_License_CM ]; then     #checks if the Adobe serialization folder exists
